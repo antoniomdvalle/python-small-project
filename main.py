19cf21c5ed6664c483ce2183ad37ddfa6258b83c
Antonio MDV <antoniomdvalle@gmail.com>
2026-01-26 14:52:42 -0300

    Adjustment of main class User

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 
-from flask import Flask
 
-
-class user(name, password, email):
+class User:
 	
 	account_type = "user"
 	
 	def __init__(self, name, password, email):
 		self.name = name
-		self.password = password
+		self._password = password
 		self.email = email
+		
+	#getter 
+	@property 
+	def email(self):
+		return self._email
+		
+	#setter
+	@email.setter
+	def email(self, value):
+		if "@" not in value:
+			raise ValueError("Invalid e-mail")
+		self._email = value
+		
+		
+u = User("John", 123132, "antonio.61conexaoxxi@gmail.com")
+
+print(f"User data: Name: {u.name}, Email: {u.email}, password: {u._password}")
 	
